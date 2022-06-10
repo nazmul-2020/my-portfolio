e2252ebd8d0fa2220db0bec2310b4d9c623499fb
nazmul-2020 <nazmulcpi2012@gmail.com>
2022-06-10 14:33:07 +0600

    added skill

--- src/Pages/Home/Skills/Skills.js
+++ src/Pages/Home/Skills/Skills.js
@@ -9,13 +9,16 @@ import node from '../../../img/skills/nodejs.svg'
 import express from '../../../img/skills/express.svg'
 import mongodb from '../../../img/skills/mongodb.svg'
 import git from '../../../img/skills/git-icon.svg'
+import netlify from '../../../img/skills/netlify.svg'
+import firebase from '../../../img/skills/firebase.svg'
+import heroku from '../../../img/skills/heroku.svg'
 import './Skills.css'
 
 const Skill = () => {
     return (
         <div className='container '>
             <h1 className='text-center'>My Skills</h1>
-            <div className='d-flex justify-content-between align-items-center w-75 mx-auto m-5'>
+            <div className='d-flex justify-content-between align-items-center w-75 mx-auto mt-5'>
                 <div className='skill'>
                     <div>
                         <img src={html} alt="" />
@@ -71,7 +74,7 @@ const Skill = () => {
                     <p className='text-center'>MongoDB</p>
                 </div>
             </div>
-            <div className='d-flex justify-content-between align-items-center w-50 mx-auto'>
+            <div className='d-flex justify-content-between align-items-center w-25 mx-auto'>
                 <div className='skill'>
                     <div className=''>
                         <img src={git} alt="" />
@@ -80,21 +83,21 @@ const Skill = () => {
                 </div>
                 <div className='skill'>
                     <div className=''>
-                        <img src={git} alt="" />
+                        <img src={netlify} alt="" />
                     </div>
-                    <p className='text-center'>GitHub</p>
+                    <p className='text-center'>Netlify</p>
                 </div>
                 <div className='skill'>
                     <div className=''>
-                        <img src={git} alt="" />
+                        <img src={firebase} alt="" />
                     </div>
-                    <p className='text-center'>GitHub</p>
+                    <p className='text-center mt-2'>Firebase</p>
                 </div>
                 <div className='skill'>
                     <div className=''>
-                        <img src={git} alt="" />
+                        <img src={heroku} alt="" />
                     </div>
-                    <p className='text-center'>GitHub</p>
+                    <p className='text-center'>Heroku</p>
                 </div>
             </div>
         </div>
